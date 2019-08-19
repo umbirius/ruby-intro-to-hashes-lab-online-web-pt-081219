2f6abc644b82c3c79efa6f0a3e2c8c26ba3220e2
Umberto Palazzo <upalazzo93@gmail.com>
2019-08-19 16:06:51 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,7 +18,12 @@ def monopoly_with_second_tier
 	  :railroads => {
 	    	  :pieces => 4,
 	    	  :names => {},
-	    	  :rent_in_dollars => {}
+	    	  :rent_in_dollars => {
+	    	    :one_piece_owned => 25, 
+	    	    :two_pieces_owned => 50,
+	    	    :three_pieces_owned => 100,
+	    	    :four_pieces_owned => 200
+	    	  }
 	  },
 
 	}
